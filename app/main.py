55fec622b642267ee662e09ebd5874f3d0553305
Yash Djson Dookun <dookun88@gmail.com>
2020-05-11 15:41:46 +0400

    DONE

--- app/main.py
+++ app/main.py
@@ -10,108 +10,112 @@ RUN_TOKEN = "thpdyEmTmWw8"
 
 
 class Data:
-	def __init__(self, api_key, project_token):
-		self.api_key = api_key
-		self.project_token = project_token
-		self.params = {
-			"api_key": self.api_key
-		}
-		self.get_data()
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token = project_token
+        self.params = {
+            "api_key": self.api_key
+        }
+        self.get_data()
 
-	def get_data(self):
-		response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data',
-								params=self.params)
-		self.data = json.loads(response.text)
+    def get_data(self):
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
+        self.data = json.loads(response.text)
 
-	def get_total_cases(self):
-		data = self.data['total']
+    def get_total_cases(self):
+        data = self.data['total']
 
-		for content in data:
-			if content['name'] == "Coronavirus Cases:":
-				return content['value']
+        for content in data:
+            if content['name'] == "Coronavirus Cases:":
+                return content['value']
 
-	def get_total_deaths(self):
-		data = self.data['total']
+    def get_total_deaths(self):
+        data = self.data['total']
 
-		for content in data:
-			if content['name'] == "Deaths:":
-				return content['value']
-		return "0"
+        for content in data:
+            if content['name'] == "Deaths:":
+                return content['value']
+        return "0"
 
-	def get_country_data(self, country):
-		data = self.data["countries"]
+    def get_country_data(self, country):
+        data = self.data["countries"]
 
-		for content in data:
-			if content['name'].lower() == country.lower():
-				return content
-		return "0"
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
+        return "0"
 
-	def get_list_of_countries(self):
-		countries = []
-		for country in self.data['countries']:
-			countries.append(country['name'].lower())
-		return countries
+    def get_list_of_countries(self):
+        countries = []
+        for country in self.data['countries']:
+            countries.append(country['name'].lower())
+        return countries
+
 
 def speak(text):
-	engine = pyttsx3.init()
-	engine.say(text)
-	engine.runAndWait()
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
 
 def get_audio():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		audio = r.listen(source)
-		said = ""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
 
-		try:
-			said = r.recognize_google(audio)
-		except Exception as e:
-			print("Exception:", str(e))
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception:", str(e))
 
-	return said.lower()
+    return said.lower()
 
 
 def main():
-	print("Started Program")
-	data = Data(API_KEY, PROJECT_TOKEN)
-	END_PHRASE = "stop"
-	country_list = data.get_list_of_countries()
+    print("Started Program")
+    data = Data(API_KEY, PROJECT_TOKEN)
+    END_PHRASE = "stop"
+    country_list = data.get_list_of_countries()
 
-	TOTAL_PATTERNS = {
-		re.compile("[\w\s]+ total [\w\s]+ cases"): data.get_total_cases,
-		re.compile("[\w\s]+ total cases"): data.get_total_cases,
-		re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
-		re.compile("[\w\s]+ total deaths"): data.get_total_deaths
-	}
+    TOTAL_PATTERNS = {
+        re.compile("[\w\s]+ total [\w\s]+ cases"): data.get_total_cases,
+        re.compile("[\w\s]+ total cases"): data.get_total_cases,
+        re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+        re.compile("[\w\s]+ total deaths"): data.get_total_deaths
+    }
 
-	COUNTRY_PATTERNS = {
-		re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
-		re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
-	}
+    COUNTRY_PATTERNS = {
+        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+        re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+    }
 
-	while True:
-		print("Listening...")
-		text = get_audio()
-		result = None
+    while True:
+        print("Listening...")
+        text = get_audio()
+        result = None
 
-		for pattern, func in COUNTRY_PATTERNS.items():
-			if pattern.match(text):
-				words = set(text.split(" "))  # turns a text like "Total deaths in USA" to {"Total", "deaths", "in", "USA"}
-				for country in country_list:
-					if country in words:
-						result = func(country)
-						break
-				break
+        for pattern, func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                words = set(
+                    text.split(" "))  # turns a text like "Total deaths in USA" to {"Total", "deaths", "in", "USA"}
+                for country in country_list:
+                    if country in words:
+                        result = func(country)
+                        break
+                break
 
-		for pattern, func in TOTAL_PATTERNS.items():
-			if pattern.match(text):
-				result = func()
-				break
+        for pattern, func in TOTAL_PATTERNS.items():
+            if pattern.match(text):
+                result = func()
+                break
 
-		if result:
-			speak(result)
+        if result:
+            speak(result)
 
-		if text.find(END_PHRASE) != -1:  # End lop
-			break
+        if text.find(END_PHRASE) != -1:  # End lop
+            break
+
+
+# Starts the Program
 main()
