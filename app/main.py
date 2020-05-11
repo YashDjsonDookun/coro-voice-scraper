ee7bab85b8034d6fcfb03b4a957f34bf6a1e7cbe
Yash Djson Dookun <dookun88@gmail.com>
2020-05-11 15:39:31 +0400

    DONE

---
+++ app/main.py
@@ -0,0 +1,117 @@
+import requests
+import json
+import pyttsx3  # text-to-speech
+import speech_recognition as sr
+import re  # regex
+
+API_KEY = "tJTWO45anuWb"
+PROJECT_TOKEN = "tL0YT1KUsBO9"
+RUN_TOKEN = "thpdyEmTmWw8"
+
+
+class Data:
+	def __init__(self, api_key, project_token):
+		self.api_key = api_key
+		self.project_token = project_token
+		self.params = {
+			"api_key": self.api_key
+		}
+		self.get_data()
+
+	def get_data(self):
+		response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data',
+								params=self.params)
+		self.data = json.loads(response.text)
+
+	def get_total_cases(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == "Coronavirus Cases:":
+				return content['value']
+
+	def get_total_deaths(self):
+		data = self.data['total']
+
+		for content in data:
+			if content['name'] == "Deaths:":
+				return content['value']
+		return "0"
+
+	def get_country_data(self, country):
+		data = self.data["countries"]
+
+		for content in data:
+			if content['name'].lower() == country.lower():
+				return content
+		return "0"
+
+	def get_list_of_countries(self):
+		countries = []
+		for country in self.data['countries']:
+			countries.append(country['name'].lower())
+		return countries
+
+def speak(text):
+	engine = pyttsx3.init()
+	engine.say(text)
+	engine.runAndWait()
+
+
+def get_audio():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+		said = ""
+
+		try:
+			said = r.recognize_google(audio)
+		except Exception as e:
+			print("Exception:", str(e))
+
+	return said.lower()
+
+
+def main():
+	print("Started Program")
+	data = Data(API_KEY, PROJECT_TOKEN)
+	END_PHRASE = "stop"
+	country_list = data.get_list_of_countries()
+
+	TOTAL_PATTERNS = {
+		re.compile("[\w\s]+ total [\w\s]+ cases"): data.get_total_cases,
+		re.compile("[\w\s]+ total cases"): data.get_total_cases,
+		re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+		re.compile("[\w\s]+ total deaths"): data.get_total_deaths
+	}
+
+	COUNTRY_PATTERNS = {
+		re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+		re.compile("[\w\s]+ deaths [\w\s]+"): lambda country: data.get_country_data(country)['total_deaths'],
+	}
+
+	while True:
+		print("Listening...")
+		text = get_audio()
+		result = None
+
+		for pattern, func in COUNTRY_PATTERNS.items():
+			if pattern.match(text):
+				words = set(text.split(" "))  # turns a text like "Total deaths in USA" to {"Total", "deaths", "in", "USA"}
+				for country in country_list:
+					if country in words:
+						result = func(country)
+						break
+				break
+
+		for pattern, func in TOTAL_PATTERNS.items():
+			if pattern.match(text):
+				result = func()
+				break
+
+		if result:
+			speak(result)
+
+		if text.find(END_PHRASE) != -1:  # End lop
+			break
+main()
